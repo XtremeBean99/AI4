1deddd64b309d27702e9a9a66eecdb6bcdea6742
XtremeBean99 <ahmedyhussain07@gmail.com>
2024-06-12 13:55:47 +1000

    Update broadcast.py
    1

--- broadcast.py
+++ broadcast.py
@@ -8,6 +8,7 @@ messages = ["Patrick: What's the password","John: My password is coolgamebro don
 
 def broadcast_message():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     server_address = ('10.13.37.255', 8000)  # Use broadcasting address and a specific port
     while True:
         for message in messages:
