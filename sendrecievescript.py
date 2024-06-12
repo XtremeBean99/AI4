e4362534e3ad6dd416a8ef5b9b846944b849d65d
XtremeBean99 <ahmedyhussain07@gmail.com>
2024-06-12 14:09:23 +1000

    Update sendrecievescript.py

--- sendrecievescript.py
+++ sendrecievescript.py
@@ -1,14 +1,13 @@
 import socket
 import base64
 
-
 def recieveAndSend():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     server_address = ('10.13.37.161', 8000)  # Use broadcasting address and a specific port
     sock.bind(server_address)
     while True:
-        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         stringData = ""
         try:
             stringData64 = base64.b64decode(data)
@@ -22,8 +21,9 @@ def recieveAndSend():
 
 def sendSecret(addr):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    server_address = (addr, 8000)  # Use broadcasting address and a specific port
+    ip_address, port = addr  # Unpack the address tuple
+    server_address = (ip_address, 8000)  # Use IP address and a specific port
     message = "cbr_CTF(assignment41357321080805508456)"
     sock.sendto(message.encode("ascii"), server_address)
+
 recieveAndSend()
