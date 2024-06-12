3b88897451e715f57154ee7caed1d7bc5781ee25
XtremeBean99 <ahmedyhussain07@gmail.com>
2024-06-12 13:57:35 +1000

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server_address = ('10.13.37.161', 8000)
 password = "coolgamebro"
 messageBytes = password.encode("ascii")
